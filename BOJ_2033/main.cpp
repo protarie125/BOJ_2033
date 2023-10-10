49fae3e322fe137c17d2318be1d26ccd7d4b468d
protarie <protarie@gmail.com>
2023-10-10 19:28:20 +0900

    초기 커밋.

---
+++ BOJ_2033/main.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	auto b = 10;
+	while (b < n) {
+		const auto r = n % b;
+		if (5 <= r / (b / 10)) {
+			n += b;
+		}
+
+		n -= r;
+		b *= 10;
+	}
+
+	cout << n;
+
+	return 0;
+}
